92a22359dd1ec94d3fdc774639599e8c2cf84f52
Matt Walston <m@mlw.ac>
2019-04-01 20:32:54 -0400

    improve ui

--- web.rb
+++ web.rb
@@ -6,6 +6,12 @@ require 'sinatra'
 require_relative 'terms'
 require_relative 'subject'
 
+class String
+  def titlecase
+    split(/([[:alpha:]]+)/).map(&:capitalize).join
+  end
+end
+
 get '/' do
   body = params['body']&.strip
   terms = []
@@ -13,7 +19,12 @@ get '/' do
   if body
     subject = Subject.new body
     terms = subject.related_to_features Terms.instance.terms
+    if terms.count > 0
+      term = "Smart #{terms&.first.titlecase}"
+    else
+      term = ''
+    end
   end
 
-  erb :index, locals: { body: body, terms: terms}
+  erb :index, locals: { body: body, term: term}
 end
